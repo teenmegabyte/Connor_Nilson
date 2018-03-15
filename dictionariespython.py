a04d62d09d08de6ebb0259d5300e1d17fdfeac1e
Connor Anthony Nilson <connor.nilson@stu.jefferson.kyschools.us>
2018-03-15 09:50:48 -0400

    Second commit

---
+++ dictionariespython.py
@@ -0,0 +1,10 @@
+Christian_vocab = {
+    "Sanctification": "to become more and more like Jesus",
+    "Justification":  "to be made righteous by God",
+    "Salvation": "the result of being saved",
+    "Glorified": "the past tense action of giving praise to God or Jesus",
+    "Hallelujah": "God be praised",
+    "Holy": "dedicated or consecrated to God or a religious purpose; sacred."
+    }
+for k,v in Christian_vocab.items():
+    print (k,v)
